05a6efd359e9f7625c3dc9fca68096f7908980a5
Ana Laura <ana.machioni@aluno.senai.br>
2026-02-20 09:00:25 -0300

    add: refazendo de acordo com os erros

--- js/app.js
+++ js/app.js
@@ -2,23 +2,29 @@
 const valor01 = document.getElementById('valor01');
 const valor02 = document.getElementById('valor02');
 // capturando o container onde o resultado sera exibido 
-const resultado = document.getElementById('resultado');
+const resultado = document.getElementById('resultado').style.display = 'flex';
+const mostrarResultado = document.getElementById('resultado').style.display = 'flex';
+// style.dysplay definindo como html e renderizando a pagina
 
 // ──────────────────────── calcular total ────────────────────────
 function calcularTotal() {
-    let valor01 = parseFloat(document.getElementById('valor01').value);
-    let valor02 = parseFloat(document.getElementById('valor02').value);
+    const preco = parseFloat(valor01.value);
+    const quantidade = parseFloat(valor02.value);
     // exibir a div resultado 
-    document.getElementById('resultado').style.display = 'flex';
-    let total = (valor01 * valor02);
+    if(isNaN (preco) || isNaN(quantidade)) {
+        mostrarResultado.textContent = 'Por favor, insira um número válido';
+        return;
+    }
+
+     const total = preco * quantidade;
 
     // capturando o container onde o resultado sera exibido 
-    const resultado = document.getElementById('resultado');
+    // const resultado = document.getElementById('resultado');
 
     resultado.innerHTML = `
-        <h2>Voce vai pagar um valor de ${total}</h2>
+        <h2>Voce vai pagar um valor de R$${total}</h2>
         <ul>
-        <li> voce esta adquirindo ${valor01} unidades com valor unitario de  ${valor02}</li>
+        <li> voce esta adquirindo R$${preco} unidades com valor unitario de  R$${quantidade}</li>
         </ul>
     `;
 
@@ -26,57 +32,60 @@ function calcularTotal() {
 
 // ──────────────────────── desconto ────────────────────────
 function desconto() {
-    let valor01 = parseFloat(document.getElementById('valor01').value);
-    let valor02 = parseFloat(document.getElementById('valor02').value);
+
+    const preco = parseFloat(valor01.value);
+    const desconto  = parseFloat(valor02.value);
 
     document.getElementById('resultado').style.display = 'flex';
 
     let desconto = (valor01 * valor02) / 100;
     let total = valor01 - desconto;
 
+    toFixed(2)
     const resultado = document.getElementById('resultado');
     resultado.innerHTML = 
     `
     <h2>Total do desconto</h2>
     <ul>
         <li>DESCONTO </li>
-        <li>Valor 01: ${valor01}</li>
-        <li>Porcentagem de desconto: ${valor02}%</li>
-        <li>Valor do desconto: ${desconto}</li>
-        <li>Valor final: ${total}</li>
+        <li>Valor 01: R$ ${valor01}</li>
+        <li>Porcentagem de desconto: R$ ${valor02}%</li>
+        <li>Valor do desconto: R$ ${desconto}</li>
+        <li>Valor final: R$ ${total}</li>
     </ul>
     `;
 }
 // ──────────────────────── juros ────────────────────────
 function juros() {
-    let valor01 = parseFloat(document.getElementById('valor01').value);
-    let valor02 = parseFloat(document.getElementById('valor02').value);
+    let valor01 = parseFloat(input01.value);
+    let valor02 = parseFloat(input02.value);
     // exibir a div resultado 
-    document.getElementById('resultado').style.display = 'flex';
+    // document.getElementById('resultado').style.display = 'flex';
 
     let acrescimo = (valor01 * valor02) / 100;
     let total = valor01 + acrescimo
     // capturando o container onde o resultado sera exibido 
-    const resultado = document.getElementById('resultado');
+    // const resultado = document.getElementById('resultado');
     resultado.innerHTML = 
-    `
-    <h2>Acréscimo aplicado</h2>
-    <ul>
-        <li>Acrescimo de: ${valor01}</li>
-        <li>sobre o valor de : ${valor02}</li>
-        <li>valor do juros ${acrescimo}</li>
-        <li>Valor final: ${total}</li>
-    </ul>
+        `
+        <h2>Acréscimo aplicado</h2>
+        <ul>
+            <li>Acrescimo de: R$${valor01}</li>
+
+            <li>sobre o valor de : R$${valor02}</li>
+            <li>valor do juros R$${acrescimo}</li>
+            <li>Valor final: R$${total}</li>
+        </ul>
     `;
 
 };
 
 // ──────────────────────── comissao ────────────────────────
 function comissao() {
-    let valor01 = parseFloat(document.getElementById('valor01').value);
-    let valor02 = parseFloat(document.getElementById('valor02').value);
+    let valor01 = parseFloat(input01.value);
+    let valor02 = parseFloat(input02.value);
 
-    document.getElementById('resultado').style.display = 'flex';
+    // document.getElementById('resultado').style.display = 'flex';
 
     let comissao = (valor02 / 100) * valor01;
 
@@ -91,8 +100,8 @@ function comissao() {
         `
         <h2>Comissão calculada</h2>
         <ul>
-            <li>Comissão de ${valor02} sobre uma venda de ${valor01}</li>
-            <h2>Valor da comissão: ${comissao}</h2>
+            <li>Comissão de R$${valor02} sobre uma venda de R$${valor01}</li>
+            <h2>Valor da comissão: R$${comissao}</h2>
         </ul>
         `};
 
@@ -100,10 +109,10 @@ function comissao() {
 
 // ──────────────────────── lucro ────────────────────────
 function lucro() {
-    let valor01 = parseFloat(document.getElementById('valor01').value);
-    let valor02 = parseFloat(document.getElementById('valor02').value);
+    let valor01 = parseFloat(input01.value);
+    let valor02 = parseFloat(input02.value);
     // exibir a div resultado 
-    document.getElementById('resultado').style.display = 'flex';
+    // document.getElementById('resultado').style.display = 'flex';
 
     let lucro = (valor01 - valor02);
 
@@ -111,13 +120,13 @@ function lucro() {
     // capturando o container onde o resultado sera exibido 
     const resultado = document.getElementById('resultado');
     resultado.innerHTML = 
-    `
-    <h2>Total lucro obtido</h2>
-    <ul>
-        <li>Preço de venda: ${valor01}</li>
-        <li>Custo: ${valor02}</li>
-        <li>Valor final: ${lucro}</li>
-    </ul>
+        `
+        <h2>Total lucro obtido</h2>
+        <ul>
+            <li>Preço de venda: R$${valor01}</li>
+            <li>Custo: R$${valor02}</li>
+            <li>Valor final: R$${lucro}</li>
+        </ul>
     `;
 
 };
@@ -125,5 +134,11 @@ function lucro() {
 // ──────────────────────── limpar ────────────────────────
 function limpar() {
     document.getElementById('resultado').style.display = 'none';
+    document.getElementById('valor01').value = '';
+    document.getElementById('valor02').value = '';
 
 };
+
+function mostrarMensagem (mensagem) {
+    mostrarResultado.textContent = mensagem
+}
